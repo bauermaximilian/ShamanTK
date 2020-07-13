96cf85fc463224ecdc41e7a54adf22a932d9fdde
Maximilian Bauer <bauermaximilian@users.noreply.github.com>
2020-07-13 21:55:22 +0200

    Updated documentation.

--- Eterra.Platforms.Windows/Controls/Controls.cs
+++ Eterra.Platforms.Windows/Controls/Controls.cs
@@ -569,22 +569,14 @@ namespace Eterra.Platforms.Windows.Controls
         }
 
         /// <summary>
-        /// Gets the characters which have been typed in in the keyboard layout
-        /// of the current user.
+        /// Gets the characters typed on the keyboard since the last update,
+        /// using the currently active keyboard layout. This does not include
+        /// any control characters beside whitespaces.
         /// </summary>
         /// <returns>
         /// A string of typed characters or an empty string, if no characters
         /// were typed or <see cref="SupportsKeyboard"/> is <c>false</c>.
         /// </returns>
-        /// <remarks>
-        /// In compliance with the behaviour of the <see cref="IControls"/>
-        /// behaviour specification (found under the remarks section of the
-        /// comment of this interface), this method will return all characters
-        /// which have been typed until the current update. After the update,
-        /// this string (or the internal structure containing the typed 
-        /// characters) can be expected to be cleared and filled with the input
-        /// following that update.
-        /// </remarks>
         public string GetTypedCharacters()
         {
             return typedCharacters.ToString();
