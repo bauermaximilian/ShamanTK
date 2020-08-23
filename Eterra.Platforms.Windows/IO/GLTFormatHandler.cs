0e7a2809df1deea77c410a3eb75e0f1b75332e75
Maximilian Bauer <bauermaximilian@users.noreply.github.com>
2020-08-23 20:30:48 +0200

    Implement mesh and skeleton import

--- Eterra.Platforms.Windows/IO/GLTFormatHandler.cs
+++ Eterra.Platforms.Windows/IO/GLTFormatHandler.cs
@@ -65,6 +65,18 @@ namespace Eterra.Platforms.Windows.IO
 
     class GLTFormatHandler : IResourceFormatHandler
     {
+        private const string VertexAttributePosition = "POSITION";
+
+        private const string VertexAttributeNormal = "NORMAL";
+
+        private const string VertexAttributeTexcoord = "TEXCOORD_0";
+
+        private const string VertexAttributeColor = "COLOR_0";
+
+        private const string VertexAttributeJoints = "JOINTS_0";
+
+        private const string VertexAttributeWeights = "WEIGHTS_0";
+
         public bool SupportsExport(string fileExtensionLowercase)
         {
             return false;
@@ -115,12 +127,257 @@ namespace Eterra.Platforms.Windows.IO
 
         private static Common.Scene GenerateScene(ModelRoot root)
         {
-            Skin skin = root.LogicalSkins.First();
-            Accessor accessor = skin.GetInverseBindMatricesAccessor();
-            var matrixArray = accessor.AsMatrix4x4Array();
-            
+            Dictionary<Mesh, MeshData> importedMeshes = 
+                new Dictionary<Mesh, MeshData>();
+
+            foreach (var node in root.LogicalNodes)
+            {
+                if (node.Mesh != null)
+                    ImportMesh(node, importedMeshes);
+            }
+
             //root.LogicalAnimations.First().FindTranslationSampler()
             throw new NotImplementedException();
+        }
+
+        private static MeshData ImportMesh(Node meshNode, 
+            Dictionary<Mesh, MeshData> previouslyImportedMeshes = null)
+        {
+            if (meshNode == null)
+                throw new ArgumentNullException(nameof(meshNode));
+            if (meshNode.Mesh == null)
+                throw new ArgumentException("The specified node doesn't " +
+                    "have a mesh that could be imported.");
+
+            if (meshNode.Mesh.Primitives.Count == 0)
+                throw new ArgumentException("The specified node contains an " +
+                    "empty mesh (mesh without primitives) that can't " +
+                    "be imported.");
+            if (meshNode.Mesh.Primitives.Count > 1)
+                Log.Warning($"The mesh node '{meshNode.Name}' contains more " +
+                    "than one mesh primitive - only the first mesh " +
+                    "primitive will be imported, the others will be ignored.",
+                        nameof(GLTFormatHandler));
+
+            MeshPrimitive meshPrimitive = meshNode.Mesh.Primitives[0];
+
+            if (meshPrimitive.DrawPrimitiveType != PrimitiveType.TRIANGLES)
+                throw new ArgumentException("The mesh of node " +
+                    $"'{meshNode.Name}' has an unsupported draw primitive " +
+                    $"type (only {nameof(PrimitiveType.TRIANGLES)} is " +
+                    "supported.");
+
+            // To prevent the same logical mesh being loaded more than once,
+            // a dictionary which contains previously imported meshes can
+            // be specified. If the dictionary contains the mesh of the
+            // current node already, its converted MeshData variant 
+            // is returned.
+            if (previouslyImportedMeshes != null)
+            {
+                if (previouslyImportedMeshes.TryGetValue(meshNode.Mesh,
+                    out MeshData mesh)) return mesh;
+            }
+
+            // For the import of the vertex data, a memory stream "wrapper" 
+            // is created around every vertex attribute channel, then we'll 
+            // "read" each stream simultaneously and assemble the data 
+            // into Vertex instances.
+            List<Vertex> vertices = new List<Vertex>();
+
+            MemoryStream getVertexData(string attributeName, out int stride)
+            {
+                Accessor accessor = meshPrimitive.GetVertexAccessor(
+                    attributeName);
+                if (accessor != null)
+                {
+                    stride = accessor.SourceBufferView.ByteStride;
+                    return new MemoryStream(
+                        accessor.SourceBufferView.Content.Array,
+                        accessor.SourceBufferView.Content.Offset,
+                        accessor.SourceBufferView.Content.Count, false);
+                }
+                else
+                {
+                    stride = 0;
+                    return null;
+                }
+            }
+
+            MemoryStream positions = getVertexData(VertexAttributePosition,
+                out int positionStride);
+            if (positions == null)
+                throw new ArgumentException("The specified node contains a " +
+                    "mesh with a primitive, but without vertex positions, " +
+                    "which is invalid and can't be imported.");
+
+            MemoryStream normals = getVertexData(VertexAttributeNormal,
+                out int normalStride);
+            MemoryStream texcoords = getVertexData(VertexAttributeTexcoord,
+                out int texcoordsStride);
+
+            MemoryStream joints = getVertexData(VertexAttributeJoints,
+                out int jointsStride);
+            MemoryStream weights = getVertexData(VertexAttributeWeights,
+                out int weightsStride);
+            MemoryStream colors = getVertexData(VertexAttributeColor,
+                out int colorsStride);
+
+            // Currently, the vertex property data can either store bone
+            // attachments (joints and weights) or colors - but not both.
+            // As the former is usually more important, the color channel is
+            // ignored in such cases.
+            if (joints != null && weights != null && colors != null)
+            {
+                Log.Warning($"The mesh node '{meshNode.Name}' contains " +
+                    "vertices with both bone attachments and a color, which " +
+                    "is currently not supported - the color channel is " +
+                    "ignored in that case.", nameof(GLTFormatHandler));
+                colors.Dispose();
+                colors = null;
+            }
+
+            VertexPropertyDataFormat vertexFormat;
+            if (joints != null && weights != null)
+                vertexFormat = VertexPropertyDataFormat.DeformerAttachments;
+            else if (colors != null)
+                vertexFormat = VertexPropertyDataFormat.ColorLight;
+            else vertexFormat = VertexPropertyDataFormat.None;
+
+            try
+            {
+                while (positions.Position < positions.Length)
+                {
+                    Vector3 position = positions.Read<Vector3>();
+                    positions?.Seek(positionStride, SeekOrigin.Current);
+
+                    Vector3 normal = normals?.Read<Vector3>() ?? default;
+                    normals?.Seek(normalStride, SeekOrigin.Current);
+
+                    Vector2 texcoord = texcoords?.Read<Vector2>() ?? default;
+                    texcoords?.Seek(texcoordsStride, SeekOrigin.Current);
+
+                    (ushort, ushort, ushort, ushort) joint = joints?
+                        .Read<ushort, ushort, ushort, ushort>() ?? default;
+                    joints?.Seek(jointsStride, SeekOrigin.Current);
+
+                    (float, float, float, float) weight = weights?
+                        .Read<float, float, float, float>() ?? default;
+                    weights?.Seek(weightsStride, SeekOrigin.Current);
+
+                    Color color = colors?.Read<Color>() ?? Color.Black;
+                    colors?.Seek(colorsStride, SeekOrigin.Current);
+
+                    VertexPropertyData vertexPropertyData;
+                    if (joints != null && weights != null)
+                        vertexPropertyData = VertexPropertyData
+                            .CreateAsDeformerAttachment(joint, weight);
+                    else if (colors != null)
+                        vertexPropertyData = VertexPropertyData
+                            .CreateAsColorLight(color, Color.Black);
+                    else vertexPropertyData = default;
+
+                    vertices.Add(new Vertex(position, normal, texcoord,
+                        vertexPropertyData));
+                }
+            }
+            finally
+            {
+                positions?.Dispose();
+                normals?.Dispose();
+                texcoords?.Dispose();
+                colors?.Dispose();
+                joints?.Dispose();
+                weights?.Dispose();
+            }
+
+            // The import of the faces is much more straightforward, luckily.
+            List<Face> faces = new List<Face>();
+            foreach ((uint, uint, uint) face in 
+                meshPrimitive.GetTriangleIndices())
+                faces.Add(new Face(face.Item1, face.Item2, face.Item3));
+
+            MeshData meshData;
+            if (vertexFormat == VertexPropertyDataFormat.DeformerAttachments)
+                meshData = MeshData.Create(vertices.ToArray(),
+                    faces.ToArray(), ImportSkeleton(meshNode, true));
+            else meshData = MeshData.Create(vertices.ToArray(),
+                    faces.ToArray(), vertexFormat);
+
+            if (previouslyImportedMeshes != null)
+                previouslyImportedMeshes[meshNode.Mesh] = meshData;
+
+            return meshData;
+        }
+
+        private static Skeleton ImportSkeleton(Node skinnedNode, 
+            bool makeReadOnly)
+        {
+            if (skinnedNode == null)
+                throw new ArgumentNullException(nameof(skinnedNode));
+            if (skinnedNode.Skin == null)
+                throw new ArgumentException("The specified node doesn't " +
+                    "have a skin that could be imported as skeleton.");
+
+            Skin skin = skinnedNode.Skin;
+
+            // The visual root of the current skinned node usually contains 
+            // both the skinned mesh node (= skinnedNode) and the 
+            // root bone node(s) - so it can be used as root for the skeleton.
+            Skeleton skeleton = new Skeleton(
+                skinnedNode.VisualRoot.LocalTransform.Matrix);            
+
+            Stack<(Node source, Node<Bone> targetParent)> conversionStack = 
+                new Stack<(Node, Node<Bone>)>();
+
+            Dictionary<Node, (Matrix4x4 offset, int boneIndex)> boneNodes =
+                new Dictionary<Node, (Matrix4x4, int)>();
+
+            for (int i = 0; i < skin.JointsCount; i++)
+            {
+                (Node boneNode, Matrix4x4 boneOffset) = skin.GetJoint(i);
+                boneNodes[boneNode] = (boneOffset, i);
+
+                // The boneNodeStack will be initialized with the root bone(s)
+                // and the skeleton (root node) as target root.
+                if (boneNode.VisualParent == boneNode.VisualRoot)
+                    conversionStack.Push((boneNode, skeleton));
+            }
+
+            while (conversionStack.Count > 0)
+            {
+                (Node source, Node<Bone> targetParent) = conversionStack.Pop();
+
+                if (boneNodes.TryGetValue(source, out var boneProperties))
+                {
+                    int boneIndex = boneProperties.boneIndex;
+                    Matrix4x4 offset = boneProperties.offset;
+
+                    if (boneIndex <= byte.MaxValue &&
+                        boneIndex >= byte.MinValue)
+                    {
+                        // The parent "skeleton node" for the children in the 
+                        // current GLTF node will either be the current 
+                        // converted GTLF node or - if the node is invalid -
+                        // the current parent "skeleton node".
+                        targetParent = targetParent.AddChild(
+                            new Bone(source.Name, (byte)boneIndex, offset));
+                    }
+                    else Log.Warning($"Bone node '{source.Name}' ignored as " +
+                        "its bone index exceeds supported/valid range.",
+                        nameof(GLTFormatHandler));
+                }
+                else Log.Warning($"Invalid node '{source.Name}' in source " +
+                        "bone hierarchy found and ignored.",
+                        nameof(GLTFormatHandler));
+
+                // Enqueue the child bone nodes to continue the traversal at
+                // a lower hierarchy level.
+                foreach (var childBoneNode in source.VisualChildren)
+                    conversionStack.Push((childBoneNode, targetParent));
+            }
+
+            if (makeReadOnly) return skeleton.ToReadOnly(false);
+            else return skeleton;
         }
 
         private static ReadContext CreateReadContext(ResourceManager manager)
